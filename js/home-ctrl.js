5c44abaecdb4d0249a02dd941e9b04956885f791
JakeAndKate2018 <soccerdudej@aol.com>
2019-01-18 18:25:55 -0600

    Added whisper of the worm Element

--- js/home-ctrl.js
+++ js/home-ctrl.js
@@ -6,6 +6,7 @@ app.controller('homeCtrl', ['$scope','$filter',function($scope, $filter) {
 	ctrl.showAC = true;
 	ctrl.showEP = true;
 	ctrl.showCurseWeek = true;
+	ctrl.showWhisper = false;
 	ctrl.curseStrength = "";
 	
 	ctrl.epRotation = [{boss:'Bok Litur, Hunger of Xol',weapon:'ALL WEAPONS', weaponImg:"images/ep/allWeapons.jpg"},
@@ -28,6 +29,9 @@ app.controller('homeCtrl', ['$scope','$filter',function($scope, $filter) {
 	}
 	ctrl.collapseCurseWeek = function(){
 		ctrl.showCurseWeek = !ctrl.showCurseWeek;
+	}
+	ctrl.collapseWhisper = function(){
+		ctrl.showWhisper = !ctrl.showWhisper;
 	}
 	
 	ctrl.previewNextWeek = function(){
@@ -70,6 +74,21 @@ app.controller('homeCtrl', ['$scope','$filter',function($scope, $filter) {
 				ctrl.curseStrength = "Low"
 				break;
 		}
+		var whisperElement = numberOfResets % 3;
+		switch (whisperElement){
+			case 0:
+				ctrl.whisperStyle = {color: 'Aqua'};
+				ctrl.whisperElement = "arc"
+				break;
+			case 1:
+				ctrl.whisperStyle = {color: 'Red'};
+				ctrl.whisperElement = "solar"
+				break;
+			case 2:
+				ctrl.whisperStyle = {color: 'Fuchsia'};
+				ctrl.whisperElement = "void"
+				break;
+		}
 			
 		ctrl.ascendantChallengeCurrent = ctrl.ascendantChallengeRotation[acRemainder];
 	}
@@ -79,15 +98,21 @@ app.controller('homeCtrl', ['$scope','$filter',function($scope, $filter) {
 		var firstReset = new Date('2019-01-01');
 		firstReset.setUTCHours(17, 0, 0, 0);
 		//how many weeks have passed since jan 1 2019
-		var weeksIn = Math.floor((Date.UTC(today.getFullYear(), today.getMonth(), today.getDate()) - Date.UTC(firstReset.getFullYear(), firstReset.getMonth(), firstReset.getDate()) ) /(1000 * 60 * 60 * 24));
-		weeksIn = weeksIn / 7;
+		var weeksIn = Math.floor((Date.UTC(today.getFullYear(), today.getMonth(), today.getDate()) - Date.UTC(firstReset.getFullYear(), firstReset.getMonth(), firstReset.getDate()) ) /(1000 * 60 * 60 * 24)) / 7;
+		var todaysReset = angular.copy(today);
+		todaysReset.setUTCHours(17, 0, 0, 0);
 		//if the day is reset day check the time
 		if(weeksIn % 1 == 0){ 
-			if(today.getHours() < firstReset.getHours()){
+			if(today.getHours() < todaysReset.getHours()){
 				weeksIn--;
 			}
 		}else{
 			weeksIn = Math.floor(weeksIn)
+		}
+		
+		if((today.getDay() == 5 && today.getHours() > todaysReset.getHours()) || today.getDay() == 6 || today.getDay() == 0 || (today.getDay() == 1 && today.getHours() < todaysReset.getHours())){
+			ctrl.whisperActive = true;
+			ctrl.showWhisper = true;
 		}
 		ctrl.weekOf.setDate(firstReset.getDate() + (weeksIn * 7));
 

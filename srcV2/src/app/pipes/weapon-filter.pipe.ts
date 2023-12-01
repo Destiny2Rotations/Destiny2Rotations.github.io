f8fe3f5838b457302c05a4f134da866c0377fec2
Destiny2rotations <soccerdudej@aol.com>
2023-12-01 15:16:13 -0500

    Filter on Season and then Source

--- srcV2/src/app/pipes/weapon-filter.pipe.ts
+++ srcV2/src/app/pipes/weapon-filter.pipe.ts
@@ -8,17 +8,24 @@ import { WeaponRoll } from '../models/weapon-roll.model';
 export class WeaponFilterPipe implements PipeTransform {
     transform(items: WeaponRoll[], filter: {search:string,season?:number,showUnobtainable?:boolean} ): any {
         items.sort((a,b)=>{
-            if(a.drop_source.name == b.drop_source.name){
-                if (a.name > b.name) {
+            if(a.season > b.season){
+                return -1
+            } else if (a.season < b.season) {
+                return 1
+            } else {
+                if(a.drop_source.name == b.drop_source.name){
+                    if (a.name > b.name) {
+                        return 1
+                    } else {
+                        return -1
+                    }
+                } else if (a.drop_source.name > b.drop_source.name) {
                     return 1
                 } else {
                     return -1
                 }
-            } else if (a.drop_source.name > b.drop_source.name) {
-                return 1
-            } else {
-                return -1
             }
+            
         })
 
         return items.filter(roll => {
